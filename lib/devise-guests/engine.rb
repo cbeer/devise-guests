586c00b71f04471388c2a6a7863ea9b43c5faab9
Chris Beer <cabeer@stanford.edu>
2012-09-20 12:11:27 -0700

    inject DeviseGuest helpers into Devise too

--- lib/devise-guests/engine.rb
+++ lib/devise-guests/engine.rb
@@ -4,8 +4,9 @@ require 'devise'
 module DeviseGuests
   class Engine < ::Rails::Engine
 
-    initializer "devise_guests.url_helpers" do
+    initializer "devise_guests.add_helpers" do
       Devise.include_helpers(DeviseGuests::Controllers)
+      Devise.helpers << DeviseGuests::Controllers::Helpers
     end
   end
 end
